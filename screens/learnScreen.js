40baa5f0995ba7cd895b6212397ac9d955e19e01
Yigit <mykayam794@gmail.com>
2025-07-01 15:07:52 +0300

    updatelearnScreen ui

--- screens/learnScreen.js
+++ screens/learnScreen.js
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import React from 'react'
 import { useSQLiteContext } from 'expo-sqlite';
 
 export default function LearnScreen() {
   const db = useSQLiteContext();
+  const swiperRef = useRef(null);
   const [words, setWords] = useState([{ "en": "phone", "id": 1, "tr": "telefon" }, { "en": "bag", "id": 2, "tr": "çanta" }, { "en": "cup", "id": 3, "tr": "bardak" }, { "en": "keyboard", "id": 4, "tr": "klavye" }]);
-
+  const [score,setScore]= useState(0);
   useEffect(() => {
     async function fetchWords() {
       const result = await db.getAllAsync(`SELECT * FROM words`);
@@ -19,19 +20,22 @@ export default function LearnScreen() {
 
     fetchWords();
   }, [])
-
-
   function onSwipedLeft() {
     console.log("Swiped Left!");
+
+
 
   }
   function onSwipedRight() {
     console.log("Swiped Right");
+
   }
   return (
     <View style={styles.container}>
+       <Text style={styles.infoText}>swipe left or right</Text>
       <Swiper
         cards={words}
+        ref={swiperRef}
         renderCard={(item) => {
           return (
             <View style={styles.card}>
@@ -44,31 +48,32 @@ export default function LearnScreen() {
         onSwiped={(cardIndex) => { console.log(cardIndex) }}
         onSwipedLeft={onSwipedLeft}
         onSwipedRight={onSwipedRight}
-        onSwipedAll={() => { console.log('onSwipedAll') }}
+        onSwipedAll={() =>{console.log("All cards swiped!")}}
         cardIndex={0}
-        backgroundColor={'#4FD0E9'}
+        backgroundColor={'#FFFFFF'}
         stackSize={4}>
-
-        <Button
-          onPress={() => { console.log('oulala') }}
-          title="Press me">
-          You can press me
-        </Button>
+        
+        
       </Swiper>
+      <Text style={styles.yourScoreHeader}>Your Score</Text>
+      <Text style={styles.score}>{score}</Text>
+      <Text style={styles.infoText}>swipe left or right</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    backgroundColor: 'red',
+
   },
   card: {
+     
     width: 200,
-    height: 100,
-    flex: 1,
+    height: 300,
+   
+    marginTop: 'auto',
     borderRadius: 42,
-    justifyContent: "center",
     alignSelf: "center",
     backgroundColor: "#115273",
     shadowColor: '#000',
@@ -78,9 +83,33 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   text: {
-    textAlign: "center",
-    fontSize: 32,
+    top: 50,
+    textAlign: 'center',
+    fontSize: 24,
     backgroundColor: "transparent",
     color: "white",
-  }
+  },
+  infoText:{
+    fontSize: 16,
+    color: 'gray',
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop: '55%',
+     
+  },
+  yourScoreHeader:{
+    fontSize: 36,
+    color: "#115273",
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop: '10%',
+  },
+  score:{
+    fontSize: 24,
+    color: "#115273",
+    alignSelf:'center',
+ position: 'absolute',
+     marginTop: '30%',
+
+    },
 })
